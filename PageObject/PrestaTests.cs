dc263d701882f85e9676b1f29c5a6d4819b095ea
Karina <karinalobova409@gmail.com>
2018-09-01 10:42:27 +0300

    Открыта главная страница сайта

---
+++ PageObject/PrestaTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObject
+{
+    [TestClass]
+    public class PrestaTests
+    {
+        public IWebDriver driver { get; set; }
+        public WebDriverWait wait { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.driver = new ChromeDriver();
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+        }
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.driver.Quit();
+        }
+        [TestMethod]
+        public void OpenSite()
+        {
+            PrestashopMainPage prestaMain = new PrestashopMainPage(this.driver);
+            prestaMain.Navigate();
+        }
+    }
+}
---
+++ PageObject/PrestashopMainPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PageObject
+{
+    public class PrestashopMainPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = @"http://prestashop-automation.qatestlab.com.ua/ru/";
+
+        public PrestashopMainPage(IWebDriver browser)
+        {
+            this.driver = browser;
+            this.driver.Manage().Window.Maximize();
+            PageFactory.InitElements(browser, this);
+        }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(url);
+            Console.WriteLine("Site is opened");
+        }
+    }
+}
